ef21ad31a6c46634e0ea7af2e7aaf23db1421fa7
Karan <karan.kumar.c@gmail.com>
2016-11-12 08:44:47 +0530

    Added a Toast message inside the onCreate()

--- app/src/main/java/com/techpalle/karan/b34gitdemo/MainActivity.java
+++ app/src/main/java/com/techpalle/karan/b34gitdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.techpalle.karan.b34gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "onCreate of MainActivity", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickAdd(View view) {
+        Toast.makeText(this, "Add button clicked", Toast.LENGTH_SHORT).show();
     }
 }
